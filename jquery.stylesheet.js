fcdd0d5d245c86b7bae3809ea269c19085dff35a
vimal.aravindashan <vimal.aravindashan@gmail.com>
2013-03-13 08:36:52 +0530

    Reversed order of evaluation of stylesheets
    Stylesheets are iterated backwards to conform to default stack like
    behaviour of CSS rules.
    Adds [].reverse to $.jQuery.fn if not already exists.
    Added doxygen comments.

--- jquery.stylesheet.js
+++ jquery.stylesheet.js
@@ -1,19 +1,39 @@
 /**
  * jQuery plugin for the stylesheet manipulation
+ * 
+ * @author Vimal Aravindashan
+ * @version 0.1.1
+ * @licensed MIT license
  */
 (function ($) {
-	var _elStyle = document.createElement('style'),
-		_styles = document.createElement('p').style,
-		vendorPrefixes = ["Webkit", "O", "Moz", "ms"];
+	var _elStyle = document.createElement('style'), /**< <style> element used as staging area for applying CSS rules */
+		_ahref = $(document.createElement('a')), /**< <a> tag used for evaluating hrefs */
+		_styles = _ahref.prop('style'), /**< Collection of styles available on the host */
+		vendorPrefixes = ["Webkit", "O", "Moz", "ms"]; /**< Case sensitive list of vendor specific prefixes */
 	
+	/**
+	 * @function filterStyleSheet
+	 * Filter a stylesheet based on ID or location
+	 * @param {String} filter Filter to be applied. id or href of the style element can be used as filters.
+	 * @param {CSSStyleSheet} styleSheet StyleSheet to be filtered
+	 * @returns {Boolean} true if stylesheet matches the filter, false otherwise
+	 */
 	function filterStyleSheet(filter, styleSheet) {
 		filter = filter || '';
 		var node = $(styleSheet.ownerNode);
 		return (filter === '') ||
 			('#'+(node.prop('id') || '') == filter) ||
-			((node.prop('href') || '') == $(document.createElement('a')).prop('href', filter).prop('href'));
+			((node.prop('href') || '') == _ahref.prop('href', filter).prop('href'));
 	}
 	
+	/**
+	 * @function vendorPropName
+	 * Vendor prefixed style property name.
+	 * Based on similar function in jQuery library.
+	 * @param {String} name camelCased CSS property name
+	 * @returns {String} Vendor specific tag @see vendorPrefixes prefixed name if found in styles @see _styles,
+	 * else passed name as-is
+	 */
 	function vendorPropName(name) {
 		var titleName = name[0].toUpperCase() + name.slice(1),
 			styleName, i = vendorPrefixes.length;
@@ -26,6 +46,19 @@
 		return name;
 	}
 	
+	/**
+	 * jQuery.stylesheet
+	 * 
+	 * Constructor/Factory method for initializing a jQuery.stylesheet object.
+	 * Includes a short-cut to apply style changes immediately.
+	 * @param {String} selector CSS rule selector text with optional stylesheet filter  
+	 * @param {String|Array|Object} name Name of style property to get/set.
+	 * Also accepts array of property names and object of name/value pairs.
+	 * @param {String} value If defined, then value of the style property
+	 * is updated with it. Unused when name is an object map.
+	 * @returns {jQuery.stylesheet|jQuery array} A new jQuery.stylesheet object if name/value is not passed,
+	 * or a jQuery array of property value pairs
+	 */
 	$.stylesheet = function (selector, name, value) {
 		if(!(this instanceof $.stylesheet)) {
 			return new $.stylesheet(selector, name, value);
@@ -36,6 +69,12 @@
 	};
 	
 	$.extend($.stylesheet, {
+		/**
+		 * @function jQuery.stylesheet.cssRules
+		 * @param {String} selector CSS rule selector text with optional stylesheet filter
+		 * @returns {Array} Array of CSSStyleRule objects that match the selector text
+		 * and pass the stylesheet filter
+		 */
 		cssRules: function (selector) {
 			if(!selector) {
 				return [];
@@ -46,7 +85,7 @@
 				styleSheetFilter = (filters.length > 1) ? $.trim(filters[0]) : '';
 			selector = (filters.length > 1) ? $.trim(filters[1].split('}')[0]) : $.trim(selector);
 			//NOTE: selector and filter will be treated as case-sensitive
-			$(document.styleSheets).not(_elStyle).each(function (i, styleSheet) {
+			$(document.styleSheets).not(_elStyle).reverse().each(function (i, styleSheet) {
 				if(filterStyleSheet(styleSheetFilter, styleSheet)) {
 					$.each((styleSheet.rules || styleSheet.cssRules), function (j, cssRule) {
 						if(cssRule instanceof CSSStyleRule && cssRule.selectorText === selector) {
@@ -58,6 +97,11 @@
 			return rules;
 		},
 		
+		/**
+		 * @function jQuery.styesheet.cssStyleName
+		 * @param {String} name Hypenated CSS property name
+		 * @returns {String} camelCased name if found in host styles, or vendor specific name
+		 */
 		cssStyleName: function (name) {
 			if(!name) {
 				return name;
@@ -69,11 +113,18 @@
 					($.cssProps[name] || ($.cssProps[name] = vendorPropName(camelName)));
 		},
 		
+		/**
+		 * @function jQuery.stylesheet.cssComputedStyles
+		 * Compress the output of jQuery.stylesheet.style() to most applicable resultant styles
+		 * NOTE: experimental feature
+		 * @param {Array} styles Array of key/value pair objects
+		 * @returns {jQuery Object} Object map of final key/value pairs
+		 */
 		cssComputedStyles: function (styles) {
 			var computedStyles = {};
 			$(styles).each(function () {
 				$.each(this, function (key, val) {
-					if(val && val.length > 0) {
+					if(val && val.length > 0 && !(key in computedStyles)) {
 						computedStyles[key] = val;
 					}
 				});
@@ -83,14 +134,32 @@
 	});
 	
 	$.extend($.fn, {
+		/**
+		 * @function jQuery.fn.cssComputedStyles
+		 * Helper function for chainability
+		 * @returns @see jQuery.stylesheet.cssComputedStyles
+		 */
 		cssComputedStyles: function () {
 			return $.stylesheet.cssComputedStyles($(this));
-		}
+		},
+		
+		/**
+		 * @function jQuery.fn.reverse
+		 * Native Object Method Array.reverse for jQuery.
+		 * Full credits to Michael Geary (http://www.mail-archive.com/discuss@jquery.com/msg04261.html)
+		 */
+		reverse: ('reverse' in $.fn) ? $.fn.reverse : [].reverse 
 	});
 	
 	$.stylesheet.fn = $.stylesheet.prototype = {
+		/**
+		 * @function jQuery.stylesheet.fn.init
+		 * Initializes a jQuery.stylesheet object.
+		 * Selects a list of applicable CSS rules for given selector @see jQuery.stylesheet.cssRules
+		 * @param {String|Array|Object} selector CSS rule selector text(s) with optional stylesheet filter(s)
+		 */
 		init: function (selector) {
-			var rules = [];
+			var rules = []; /**< Array of CSSStyleRule objects matching the selector initialized with */
 			
 			switch($.type(selector)) {
 			case 'string':
@@ -113,10 +182,23 @@
 			}
 			
 			$.extend(this, {
+				/**
+				 * @function jQuery.stylesheet.rules
+				 * @returns {Array} Copy of array of CSSStyleRule objects used by this instance of jQuery.stylesheet 
+				 */
 				rules: function() {
 					return rules.slice();
 				},
 				
+				/**
+				 * @function jQuery.stylesheet.style()
+				 * @param {String|Array|Object} name Name of style property to get/set.
+				 * Also accepts array of property names and object of name/value pairs.
+				 * @param {String} value If defined, then value of the style property
+				 * is updated with it. Unused when name is an object map.
+				 * @returns {jQuery.stylesheet|jQuery array} A new jQuery.stylesheet object if name/value is not passed,
+				 * or a jQuery array of property value pairs
+				 */
 				style: function (name, value) {
 					var self = this, styles = $([]);
 					
